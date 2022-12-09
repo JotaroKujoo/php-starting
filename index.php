eba0f6179c89d13e1bb8561c1455bb760825d7a6
Jose Rodriguez <rdrgz.jose123@gmail.com>
2022-12-09 21:29:22 +0100

    Training with php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+echo 'Esto es una prueba';
+echo "<br>
+        <html>
+        <body>
+        <h1> Prueba de a ver cuantas cosas se traga la puerca de PHP </h1>
+        <h2> Resultado: Por el momento todo lo que le pongas </h2>
+        </body>
+        </html>";
+?>
+
+
+<?php 
+echo 'Esto es una prueba';
+?>
+
+<br>
+
+
+<?php 
+echo 'Hemos omitido la última etiqueta de cierre'; 
+?>
